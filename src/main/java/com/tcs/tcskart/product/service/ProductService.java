7182de0ed5f1ab464369348844d8f449c0fb87ab
anusha <anushathandrapati@gmail.com>
2025-07-03 05:12:00 +0000

    updated product service

--- src/main/java/com/tcs/tcskart/product/service/ProductService.java
+++ src/main/java/com/tcs/tcskart/product/service/ProductService.java
@@ -3,9 +3,11 @@ package com.tcs.tcskart.product.service;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 
 import com.tcs.tcskart.product.entity.Product;
 import com.tcs.tcskart.product.entity.ProductReview;
+import com.tcs.tcskart.product.utility.ProductCategory;
 //import com.tcs.tcskart.product.entity.ProductReview;
 
 public interface ProductService {
@@ -26,12 +28,12 @@ public interface ProductService {
 
 	void deleteProductByName(String productName);
 
-
+	public List<Product> searchByProductCategory(ProductCategory productCategory);
 	Page<Product> getPaginatedProducts(int page, int size);
 
 	Product updateProductById(Integer productId, Product product);
-
-	void deleteProductByID(Integer productId);
+	
+	ResponseEntity<String> deleteById(Integer productId);
 	public String addProductReview(String email, int productId, double rating, String reviewText);
 	public List<ProductReview> getReviewsByProductId(int productId);
 	  public List<Product> getProductsSortedByRating();
